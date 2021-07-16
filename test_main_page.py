11dbc10c8e4a6fb297710ac2157e6e153fda8fc6
AntonPenkin <razvetchik5@gmail.com>
2021-07-16 06:44:12 +0300

    Added tests for checking login and registration forms

--- test_main_page.py
+++ test_main_page.py
@@ -1,15 +1,18 @@
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
 
 
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com"
+    link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
+    page_login = LoginPage(browser, link)
     page.open()
     page.go_to_login_page()
+    page_login.should_be_login_page()
 
 
 def test_guest_should_see_login_link(browser):
-    link = "http://selenium1py.pythonanywhere.com"
+    link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
