ad409f95cb0c1b3a3743134a0c1396edfd2f330f
AntonPenkin <razvetchik5@gmail.com>
2021-07-22 19:46:19 +0300

    Made some changes

--- pages/base_page.py
+++ pages/base_page.py
@@ -8,10 +8,9 @@ import math
 
 
 class BasePage:
-    def __init__(self, browser, url, timeout=10):
+    def __init__(self, browser, url):
         self.browser = browser
         self.url = url
-        self.browser.implicitly_wait(timeout)
 
     # Данный метод принимает аргумент в виде ссылки и открывает её
     def open(self):
